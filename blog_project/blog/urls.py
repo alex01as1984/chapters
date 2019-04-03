e2c36d35c21c4f1a125486ddb6f5dc74b0f32357
alex01as1984 <49124051+alex01as1984@users.noreply.github.com>
2019-04-03 22:34:15 +0500

    forms /create/edit/delete/

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView     # new
+from .views import (
+    BlogListView, 
+    BlogDetailView, 
+    BlogCreateView,     # ссылка на создание
+    BlogUpdateView,     # сслыка на обновление
+    BlogDeleteView,     # сслыка на удаление
+)
 
 urlpatterns = [
-    path('post/new/', BlogCreateView.as_view(), name='post_new'),
-    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail' ), # new
-    path('', BlogListView.as_view(), name='home'),  # new 
+    path('post/<int:pk>/edit/', 
+        BlogUpdateView.as_view(), name='post_edit'),                    # new 4
+    path('post/<int:pk>/delete/',
+        BlogDeleteView.as_view(), name='post_delete'),                  # new 5 
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),           # new 3 сслыка для создания поста
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail' ), # new 2 ссылка для детализации поста (зайти в пост)
+    path('', BlogListView.as_view(), name='home'),  # new 1 ссылка для отображения начатльной страницы с потами
 ]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,19 +1,34 @@
 from django.views.generic import ListView, DetailView   # new
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView # new
 from .models import Post
+from django.urls import reverse_lazy    # new  to do delele
 
 # Create your views here.
 
 class BlogListView(ListView):
     model = Post
-    template_name = 'home.html'
+    template_name = 'home.html'         # какой шаблон подгружать. базовый 
 
+# класс для детализации поста
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
 
+# класс для создания поста
 class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+# класс для обновления поста
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'            # шаблон подгружать
+    fields = ['title','author', 'body']         # какие поля выводить для обновления
+
+# класс для удаления поста
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'      # шаблон какой подгружать
+    success_url = reverse_lazy('home')      # возврат на старницу home
