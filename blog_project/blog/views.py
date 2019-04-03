72e05f919dfc11d23fbaad286cac42cbe1ecb206
alex01as1984 <49124051+alex01as1984@users.noreply.github.com>
2019-04-03 19:38:52 +0500

    base blog

---
+++ blog_project/blog/views.py
@@ -0,0 +1,19 @@
+from django.views.generic import ListView, DetailView   # new
+from django.views.generic.edit import CreateView
+from .models import Post
+
+# Create your views here.
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
